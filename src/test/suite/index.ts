1fff2a1aa245b77d3bbe9f6fad54e47d3ada13a3
Nicolas LIGER <nicolas.liger@gmail.com>
2025-01-03 12:58:38 +0100

    Add last dependencies

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,38 +1,36 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { glob } from 'glob';
 
 export function run(): Promise<void> {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		color: true
-	});
+    // Crée une instance de Mocha
+    const mocha = new Mocha({
+        ui: 'tdd',
+        color: true,
+    });
 
-	const testsRoot = path.resolve(__dirname, '..');
+    const testsRoot = path.resolve(__dirname, '..');
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+    return new Promise(async (resolve, reject) => {
+        try {
+            // Utiliser l'API asynchrone de glob
+            const files = await glob('**/**.test.js', { cwd: testsRoot });
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+            // Ajouter les fichiers de test à Mocha
+            files.forEach((f: string) =>
+                mocha.addFile(path.resolve(testsRoot, f)),
+            );
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
-	});
+            // Exécuter les tests
+            mocha.run((failures) => {
+                if (failures > 0) {
+                    reject(new Error(`${failures} tests failed.`));
+                } else {
+                    resolve();
+                }
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
 }
